0c982b16e4b8387d52847862fdc028f9de659e14
diandrade <contato.andradediego@gmail.com>
2025-03-31 16:42:49 -0300

    Finalização do desafio Java

--- primeiraAplicacao/src/Desafio.java
+++ primeiraAplicacao/src/Desafio.java
@@ -32,7 +32,7 @@ public class Desafio {
         }
 
         // Validação do saldo (número positivo)
-        int saldoInicial;
+        double saldoInicial;
         while (true) {
             System.out.println("Insira seu saldo inicial: ");
             try {
@@ -43,7 +43,7 @@ public class Desafio {
                 System.out.println("Saldo deve ser positivo!");
             } catch (InputMismatchException e) {
                 System.out.println("Insira um número válido!");
-                scanner.next();  // Limpa o input inválido
+                scanner.next();
             }
         }
 
@@ -51,22 +51,51 @@ public class Desafio {
         scanner.close();
     }
 
-    private static void painel(String nome, String tipoDeConta, int saldoInicial) {
-        System.out.println("*************************************");
-        System.out.println("Dados iniciais do cliente:");
-        System.out.println();
-        System.out.println("Nome:           " + nome);
-        System.out.println("Tipo Conta:     " + tipoDeConta);
-        System.out.println("Saldo Inicial:  R$" + saldoInicial);
-        System.out.println("*************************************");
-        System.out.println();
-        System.out.println();
-        System.out.println("Operações");
-        System.out.println();
-        System.out.println("1 - Consultar Saldos");
-        System.out.println("2 - Receber Valor");
-        System.out.println("3 - Transferir Valor");
-        System.out.println("4 - Sair");
-    }
+    public static void painel(String nome, String tipoDeConta, double saldoInicial) {
+        Scanner scanner = new Scanner(System.in);
 
+        boolean loop = true;
+        while (loop) {
+            System.out.println("\n*************************************");
+            System.out.println("\nDados iniciais do cliente:");
+            System.out.println("Nome:           " + nome);
+            System.out.println("Tipo Conta:     " + tipoDeConta);
+            System.out.println("Saldo Inicial:  R$" + saldoInicial);
+            System.out.println("*************************************");
+            System.out.println("\nOperações");
+            System.out.println("\n1 - Consultar Saldos");
+            System.out.println("2 - Receber Valor");
+            System.out.println("3 - Transferir Valor");
+            System.out.println("4 - Sair");
+            System.out.println("\nInsira a opção desejada: ");
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Seu saldo atual é: " + saldoInicial);
+                    break;
+                case 2:
+                    System.out.println("Insira a quantidade de valor a ser recebida: ");
+                    double valorRecebido = scanner.nextDouble();
+                    saldoInicial += valorRecebido;
+                    break;
+                case 3:
+                    System.out.println("Insira a quantidade de valor a ser transferida: ");
+                    double valorTransferido = scanner.nextDouble();
+                    if (valorTransferido > saldoInicial) {
+                        System.out.println("Impossível realizar esta transferência.");
+                    } else {
+                        saldoInicial -= valorTransferido;
+                    }
+                    break;
+                case 4:
+                    loop = false;
+                    System.out.println("Saindo....");
+                    break;
+                default:
+                    loop = false;
+                    System.out.println("Insira uma opção válida.");
+            }
+        }
+    }
 }
